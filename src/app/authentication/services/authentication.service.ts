659714256ed801589d55fdc4d3d05cffcacbf598
vinicius rodrigues <vinitecnico@gmail.com>
2019-03-06 21:38:49 -0300

    Update authentication.service.ts

--- src/app/authentication/services/authentication.service.ts
+++ src/app/authentication/services/authentication.service.ts
@@ -1,27 +1,25 @@
 import { Injectable, Inject, OnInit } from '@angular/core';
+import { Apollo } from 'apollo-angular';
+import * as Query from '../query/global-query';
 import { map } from 'rxjs/operators';
 import { Router } from '@angular/router';
 
 @Injectable()
 export class AuthenticationService {
     constructor(private router: Router,
+                 private apollo: Apollo,
         @Inject('LocalStorage') localStorage) {
     }
 
     login(authData: {username: string, password: string}) {
-        // this.itemsCollection = this.db.collection('user', ref =>
-        //     ref.where('username', '==', authData.username)
-        //         .where('password', '==', authData.password));
-        // return this.itemsCollection.snapshotChanges()
-        //     .pipe(
-        //         map(actions =>
-        //             actions.map(a => {
-        //                 const data = a.payload.doc.data() as any;
-        //                 data.id = a.payload.doc.id;
-        //                 return data;
-        //             })
-        //         )
-        //     );
+        return this.apollo
+            .mutate({
+                mutation: Query.deliveryPrice,
+                variables: {
+                    zipcode: zipcode,
+                    totalPrice: totalPrice
+                }
+            });
     }
 
     checkLocalData() {
